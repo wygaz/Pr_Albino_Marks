29df31ad1798a6dcdd03aa978021356cc86de603
Wanderley Gazeta <wygazeta@gmail.com>
2025-10-22 22:21:16 -0300

    resolvendo problema de redirecionamento do favicon.icol

--- pralbinomarks/urls.py
+++ pralbinomarks/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('A_Lei_no_NT.urls', 'A_Lei_no_NT'), namespace='A_Lei_no_NT')),
+'''
     path("favicon.ico", RedirectView.as_view(
         url=static("favicon.ico"),
         permanent=False,  # evita cache duro de 301
     )),
 ]
-
+'''
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
